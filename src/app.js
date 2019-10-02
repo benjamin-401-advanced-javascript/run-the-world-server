6f584b7a9a679d989b68215f41fcad3e017463fb
Benjamin Clark <benisin@gmail.com>
2019-10-01 19:49:18 -0700

    User model

--- src/app.js
+++ src/app.js
@@ -1,18 +1,23 @@
 
-
 const express = require('express');
-
+const app = express();
 
 const morgan = require('morgan');
 const cors = require('cors');
 
-const app = express();
+const errorHandler = require('./middleware/server-error.js');
+const notFound = require('./middleware/not-found.js');
 
+// ROUTER
+const authRouter = require('./route/auth.js');
+
+// MIDDLEWARE
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 
-
+// OUR MIDDLEWARE
+app.use(authRouter);
 app.use('*', notFound);
 app.use(errorHandler);
 
