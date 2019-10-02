750cb7db012786f8ef784acbf009ce5b44ee33a9
Benjamin Clark <benisin@gmail.com>
2019-10-02 16:37:18 -0700

    mongoose model

--- src/model/user/schema.js
+++ src/model/user/schema.js
@@ -4,12 +4,16 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
+const SINGLE_USE_TOKENS = !!process.env.SINGLE_USE_TOKENS;
+const TOKEN_EXPIRE = process.env.TOKEN_LIFETIME || '5m';
 const SECRET = process.env.SECRET || 'foobar';
 
+const usedTokens = new Set();
 
 const user = mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  email: { type: String },
   role: { type: String, default: 'user', enum: ['admin', 'editor', 'user'] },
 });
 
@@ -19,24 +23,44 @@ const capabilities = {
   user: ['read'],
 };
 
-// hash password before saving
 user.pre('save', async function () {
   if (this.isModified('password')) {
     this.password = await bcrypt.hash(this.password, 10);
   }
 });
 
-// Bearer Auth
+user.statics.createFromOauth = function (email) {
+
+  if (!email) { return Promise.reject('Validation Error'); }
+
+  return this.findOne({ email })
+    .then(user => {
+      if (!user) { throw new Error('User Not Found'); }
+      return user;
+    })
+    .catch(error => {
+      let username = email;
+      let password = 'none';
+      return this.create({ username, password, email });
+    });
+
+};
+
 user.statics.authenticateToken = function (token) {
+
+  if (usedTokens.has(token)) {
+    return Promise.reject('Invalid Token');
+  }
+
   try {
     let parsedToken = jwt.verify(token, SECRET);
-    // (SINGLE_USE_TOKENS) && parsedToken.type !== 'key' && usedTokens.add(token);
+    (SINGLE_USE_TOKENS) && parsedToken.type !== 'key' && usedTokens.add(token);
     let query = { _id: parsedToken.id };
     return this.findOne(query);
   } catch (e) { throw new Error('Invalid Token'); }
+
 };
 
-// Basic Auth
 user.statics.authenticateBasic = function (auth) {
   let query = { username: auth.username };
   return this.findOne(query)
@@ -49,7 +73,6 @@ user.methods.comparePassword = function (password) {
     .then(valid => valid ? this : null);
 };
 
-
 user.methods.generateToken = function (type) {
 
   let token = {
@@ -58,18 +81,20 @@ user.methods.generateToken = function (type) {
     type: type || 'user',
   };
 
-  // let options = {};
-  // if (type !== 'key' && !!TOKEN_EXPIRE) {
-  //   options = { expiresIn: TOKEN_EXPIRE };
-  // }
+  let options = {};
+  if (type !== 'key' && !!TOKEN_EXPIRE) {
+    options = { expiresIn: TOKEN_EXPIRE };
+  }
 
-  return jwt.sign(token, SECRET);
+  return jwt.sign(token, SECRET, options);
 };
 
-
-// Does a capability exist for this user role
 user.methods.can = function (capability) {
   return capabilities[this.role].includes(capability);
 };
 
-module.exports = mongoose.model('User', user);
+user.methods.generateKey = function () {
+  return this.generateToken('key');
+};
+
+module.exports = mongoose.model('user', user);
