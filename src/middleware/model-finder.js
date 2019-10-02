0fd2eac7fc3e391f43e63940ee8116ef8de9d545
Benjamin Clark <benisin@gmail.com>
2019-10-02 16:38:14 -0700

    model finder middleware

--- src/middleware/model-finder.js
+++ src/middleware/model-finder.js
@@ -5,4 +5,4 @@ module.exports = (request, response, next) => {
   // what are req.params "http://localhost:8080/v1/api/model/:modelName"
   request.model = require(`../model/${modelName}`);
   next();
-}
+};
