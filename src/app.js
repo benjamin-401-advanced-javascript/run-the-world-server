311c90f50879c5371d173686a885054aad65a5ee
Benjamin Clark <benisin@gmail.com>
2019-10-01 23:09:54 -0700

    updating to get travis to run

--- src/app.js
+++ src/app.js
@@ -8,15 +8,15 @@ const cors = require('cors');
 const errorHandler = require('./middleware/server-error.js');
 const notFound = require('./middleware/not-found.js');
 
-// ROUTER
+// - ROUTER
 const authRouter = require('./route/auth.js');
 
-// MIDDLEWARE
+// - MIDDLEWARE
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 
-// OUR MIDDLEWARE
+// - OUR MIDDLEWARE
 app.use(authRouter);
 app.use('*', notFound);
 app.use(errorHandler);
